d4535c791859af04ad3bd3efb18d97227d07454a
Joé Veber <joeveber@Joes-MacBook-Pro.local>
2022-04-24 23:48:22 +0200

    Frontend update 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Document chargé");
     const contactForm = document.querySelector("#contactForm");
-    contactForm.addEventListener("submit", (event) => {
+    contactForm.addEventListener("submit", async (event) => {
         event.preventDefault();
         const data = {
             firstname: document.querySelector("#firstname").value,
             lastname: document.querySelector("#lastname").value,
             email: document.querySelector("#email").value,
-            message: document.querySelector("#description").value,
+            message: document.querySelector("#message").value,
         }
-        console.log(data);
+        console.log(data); 
+        const response = await axios.post("http://localhost:3000/",data);
+        console.log(response);
+        if (response.status !== 200) {
+            alert("The form couldn't be send");
+        } else {
+            alert("The form has been sent");
+        }
     });
 });
