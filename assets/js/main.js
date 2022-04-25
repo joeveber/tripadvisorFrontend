0ef3a5c6d40df95d23436d99bfa5e9f1b37e6e27
Joé Veber <joeveber@Joes-MacBook-Pro.local>
2022-04-25 17:00:49 +0200

    updating once more

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Document chargé");
     const contactForm = document.querySelector("#contactForm");
+
+    const button = document.querySelector("button")
+
     contactForm.addEventListener("submit", async (event) => {
         event.preventDefault();
+
+        button.setAttribute("disabled", "disabled");
+        button.classList.add("btn-disabled");
+
         const data = {
             firstname: document.querySelector("#firstname").value,
             lastname: document.querySelector("#lastname").value,
@@ -11,12 +18,16 @@ document.addEventListener("DOMContentLoaded", () => {
             message: document.querySelector("#message").value,
         }
         console.log(data); 
-        const response = await axios.post("http://localhost:3000/",data);
+        const response = await axios.post("https://tripadvisor-form.herokuapp.com/",data);
         console.log(response);
         if (response.status !== 200) {
             alert("The form couldn't be send");
+            button.removeAttribute("disabled");
+            button.classList.remove("btn-disabled");
         } else {
             alert("The form has been sent");
+            button.removeAttribute("disabled");
+            button.classList.remove("btn-disabled");
         }
     });
 });
